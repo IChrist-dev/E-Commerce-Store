99bb38327f129513f30aa8c31d943b6ac81489b4
Ian Christian <W0480449@campus.nscc.ca>
2023-11-06 10:20:31 -0400

    Add project files.

---
+++ INET_2005_Final_Project/Models/Condition.cs
@@ -0,0 +1,8 @@
+﻿namespace INET_2005_Final_Project.Models
+{
+    public class Condition
+    {
+        public int ConditionId { get; set; }
+        public string ConditionName { get; set; } = string.Empty;
+    }
+}
---
+++ INET_2005_Final_Project/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace INET_2005_Final_Project.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; } = string.Empty;
+        public string FileName { get; set; } = string.Empty;
+        public double Price { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public int Runtime { get; set; }
+        public Condition Condition { get; set; } = new();
+        public int ConditionId { get; set; }
+    }
+}
