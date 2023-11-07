13007890c31d74d82887795195a8b323fd8231f2
W0480449 <w0480449@nscc.ca>
2023-11-07 13:00:23 -0400

    Image upload for create page works

--- INET_2005_Final_Project/Models/Product.cs
+++ INET_2005_Final_Project/Models/Product.cs
@@ -1,13 +1,21 @@
-﻿namespace INET_2005_Final_Project.Models
+﻿using System.ComponentModel;
+
+namespace INET_2005_Final_Project.Models
 {
     public class Product
     {
         public int ProductId { get; set; }
+
+        [DisplayName("Product Name")]
         public string ProductName { get; set; } = string.Empty;
+
+        [DisplayName("Cover Art")]
         public string FileName { get; set; } = string.Empty;
         public decimal Price { get; set; }
         public string Description { get; set; } = string.Empty;
         public bool isMovie { get; set; }
+
+        [DisplayName("Runtime (m)")]
         public int Runtime { get; set; }
         public string Condition { get; set; } = string.Empty;
     }
--- INET_2005_Final_Project/Pages/Products/Create.cshtml.cs
+++ INET_2005_Final_Project/Pages/Products/Create.cshtml.cs
@@ -13,6 +13,7 @@ namespace INET_2005_Final_Project.Pages.Products
     public class CreateModel : PageModel
     {
         private readonly INET_2005_Final_ProjectContext _context;
+        IWebHostEnvironment _env;
 
         [BindProperty]
         public Product Product { get; set; } = default!;
@@ -20,9 +21,30 @@ namespace INET_2005_Final_Project.Pages.Products
         [BindProperty]
         public IFormFile ImageUpload { get; set; } = null!;
 
-        public CreateModel(INET_2005_Final_ProjectContext context)
+        public List<string> ConditionList { get; set; } = new();
+
+        public List<SelectListItem> SelectConditionList { get; set; } = new();
+
+        public CreateModel(INET_2005_Final_ProjectContext context, IWebHostEnvironment env)
         {
             _context = context;
+            _env = env;
+
+            // Hard-coded condition names cannot be modified
+            ConditionList.Add("Like New");
+            ConditionList.Add("Good");
+            ConditionList.Add("Fair");
+            ConditionList.Add("Some Wear");
+            ConditionList.Add("Poor");
+
+            for (int i = 0; i < ConditionList.Count; i++)
+            {
+                SelectConditionList.Add(new SelectListItem()
+                {
+                    Value = i.ToString(),
+                    Text = ConditionList[i]
+                });
+            }
         }
 
         public IActionResult OnGet()
@@ -34,13 +56,33 @@ namespace INET_2005_Final_Project.Pages.Products
         // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
         public async Task<IActionResult> OnPostAsync()
         {
-          if (!ModelState.IsValid || _context.Product == null || Product == null)
+
+            // Convert Condition from index to simple string value
+            int selectedConditionIndex = int.Parse(Product.Condition);
+            Product.Condition = ConditionList.ElementAt(selectedConditionIndex);
+
+            // Make a unique image name and set for product
+            string imageName = DateTime.Now.ToString("yyyy_MM_dd_hh_mm_ss-") + ImageUpload.FileName;
+
+            Product.FileName = imageName;
+
+            // Validate model
+            if (!ModelState.IsValid || _context.Product == null || Product == null)
             {
                 return Page();
             }
 
+            // Save to database
             _context.Product.Add(Product);
             await _context.SaveChangesAsync();
+
+            // Assuming successful DB insert, save image file to public folder
+            string file = Path.Combine(_env.ContentRootPath, "wwwroot\\photos\\", imageName);
+
+            using (FileStream fs = new FileStream(file, FileMode.Create))
+            {
+                ImageUpload.CopyTo(fs);
+            }
 
             return RedirectToPage("./Index");
         }
