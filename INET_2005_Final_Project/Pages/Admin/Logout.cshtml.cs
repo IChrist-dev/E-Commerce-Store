b7a20e223717df922ac3eb06646c3a2cbbace501
Ian Christian <w0480449@nscc.ca>
2023-11-09 12:08:21 -0400

    Logout also works

--- INET_2005_Final_Project/Pages/Admin/Logout.cshtml.cs
+++ INET_2005_Final_Project/Pages/Admin/Logout.cshtml.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +7,11 @@ namespace INET_2005_Final_Project.Pages.Admin
 {
     public class LogoutModel : PageModel
     {
-        public void OnGet()
+        public async Task<IActionResult> OnGet()
         {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+
+            return RedirectToPage("/Admin/Login");
         }
     }
 }
